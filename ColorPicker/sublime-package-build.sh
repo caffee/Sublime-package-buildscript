b0f0a7461cc712ee104f7a94ef66708756aa6341
caffee <lee.github@gmail.com>
2013-09-08 21:32:06 +0800

    add ColorPicker sublime-package build script

---
+++ ColorPicker/sublime-package-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+zip -r ColorPicker.sublime-package\
+       lib\
+       sublimecp.py\
+       Default\ \(Windows\).sublime-keymap\
+       Default\ \(OSX\).sublime-keymap\
+       Default\ \(Linux\).sublime-keymap\
+       ColorPicker.sublime-commands\
+       README.md
