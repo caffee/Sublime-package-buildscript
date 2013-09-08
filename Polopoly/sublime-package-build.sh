17e580fba22b7d0d08cca629fe227a65bb7ff38c
caffee <lee.github@gmail.com>
2013-09-08 21:31:29 +0800

    add Polopoly.sublime-package build script

---
+++ Polopoly/sublime-package-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+zip -r Polopoly.sublime-package\
+       Snippets\
+       Syntax\
+       dot_content_completions.py\
+       p_fields_layouts_completions.py\
+       LICENSE\
+       README.rst
