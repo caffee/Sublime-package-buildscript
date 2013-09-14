8171b50e6f12455a9b95acc2e1e88b5c9d7dd783
caffee <lee.github@gmail.com>
2013-09-14 22:14:08 +0800

    add ThemeSoda sublime-package build script

---
+++ ThemeSoda/sublime-package-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+zip -r Theme\ -\ Soda.sublime-package\
+       Soda\ Dark\
+       Soda\ Light\
+       Soda\ Dark\ 3.sublime-theme\
+       Soda\ Dark.sublime-theme\
+       Soda\ Light\ 3.sublime-theme\
+       Soda\ Light.sublime-theme\
+       README.md
