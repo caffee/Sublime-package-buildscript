10563339d0c7369140e5d34b615ac17dab2562b1
caffee <lee.github@gmail.com>
2013-09-22 02:28:27 +0800

    update Polopoly.sublime-package build script

--- Polopoly/sublime-package-build.sh
+++ Polopoly/sublime-package-build.sh
@@ -2,8 +2,15 @@
 
 zip -r Polopoly.sublime-package\
        Snippets\
-       Syntax\
-       dot_content_completions.py\
+       Velocity-Polopoly.sublime-completions\
+       Velocity-Polopoly.sublime-settings\
+       Velocity-Polopoly.tmLanguage\
+       velocity_polopoly_completions.py\
+       XML-Polopoly.tmLanguage\
+       XML-Polopoly.sublime-settings\
        p_fields_layouts_completions.py\
+       dotContent-Polopoly.tmLanguage\
+       dot_content_completions.py\
        LICENSE\
-       README.rst
+       RELEASE_NOTES.md\
+       README.md
